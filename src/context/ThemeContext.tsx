9a2f7748c65dd63335308fd3a4f68f85b32b814e
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-09 15:48:02 -0300

    created themeContext

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { createContext, useEffect, useState } from "react";
+
+interface ThemeContextType {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }: Props) => {
+  const [theme, setTheme] = useState<string>(
+    localStorage.getItem("theme") ?? "light"
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    const oldTheme = theme === "dark" ? "light" : "dark";
+
+    root.classList.remove(oldTheme);
+    root.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
