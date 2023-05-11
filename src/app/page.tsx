68e7b7eb2bad466d276f79ca2186248e028b4611
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-11 18:29:42 -0300

    importing AuthForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
+import AuthForm from "@/components/AuthForm";
+import Text from "@/components/Text";
+import ThemeButton from "@/components/ThemeButton";
+import Image from "next/image";
+
 export default function Home() {
-  return <h1 className="text-center font-bold text-2xl">Hello!!</h1>;
+  return (
+    <>
+      <header className="py-2 px-4 w-full bg-gray-100 flex justify-between shadow-xl items-center transition-all dark:bg-slate-800">
+        <Image
+          src="/images/logo.png"
+          alt="Logo"
+          height={300}
+          width={300}
+          className="w-14 -m-2"
+        />
+        <ThemeButton />
+      </header>
+      <main className="flex flex-col min-h-full justify-center py-12 sm:px-6 lg:px-8 bg-gray-200 transition-all dark:bg-slate-900">
+        <div className="-mt-10 sm:mx-auto sm:w-full sm:max-w-md">
+          <Text
+            paragraph
+            className=" text-center text-2xl font-bold tracking-tight"
+          >
+            Sign in to your account!
+          </Text>
+          <AuthForm />
+        </div>
+      </main>
+    </>
+  );
 }
