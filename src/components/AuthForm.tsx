3f0e6b626572bf7d861411b30b4637c57e86ce73
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-14 19:37:01 -0300

    login github

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -63,6 +63,17 @@ const AuthForm: FunctionComponent = () => {
 
   const socialAction = (action: string) => {
     setIsLoading(true);
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("Invalid credentials!");
+        }
+
+        if (callback?.ok && !callback?.error) {
+          toast.success("Logged in!");
+        }
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
