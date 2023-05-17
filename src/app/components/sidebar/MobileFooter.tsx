c0154d91d7e4f5ac2759d25a03091ab31c87425a
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-16 21:29:20 -0300

    creating MobileFooter component

---
+++ src/app/components/sidebar/MobileFooter.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import useConversation from "@/hooks/useConversation";
+import useRoutes from "@/hooks/useRoutes";
+import MobileItem from "./MobileItem";
+
+const MobileFooter = () => {
+  const routes = useRoutes();
+  const { isOpen } = useConversation();
+
+  if (isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="fixed flex items-center justify-around w-full py-1 bottom-0 z-40 bg-gray-200 border-t-[1px] border-gray-300 lg:hidden">
+      {routes.map((item) => (
+        <MobileItem
+          key={item.label}
+          href={item.href}
+          active={item.active}
+          icon={item.icon}
+          onClick={item.onClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default MobileFooter;
