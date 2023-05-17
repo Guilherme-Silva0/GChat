e5e59d13190537ed6769a2045b30a354d507457a
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-16 21:23:30 -0300

    creating page component

---
+++ src/app/users/page.tsx
@@ -0,0 +1,9 @@
+import StateEmpty from "@/app/components/StateEmpty";
+
+export default function Users() {
+  return (
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <StateEmpty />
+    </div>
+  );
+}
