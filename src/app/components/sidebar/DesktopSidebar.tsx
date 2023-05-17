32a8f341ac522e09a907a635ffcb24e611b41a2e
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-17 13:18:10 -0300

    adding user avatar

--- src/app/components/sidebar/DesktopSidebar.tsx
+++ src/app/components/sidebar/DesktopSidebar.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import useRoutes from "@/hooks/useRoutes";
+import { User } from "@prisma/client";
 import { useState } from "react";
+import Avatar from "../Avatar";
 import DesktopItem from "./DesktopItem";
 
-const DesktopSidebar = () => {
+const DesktopSidebar = ({ user }: { user: User }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-gray-200 dark:lg:bg-slate-900 lg:border-r-[1px] lg:border-gray-300 lg:pb-4 lg:flex lg:flex-col justify-between">
+    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-gray-200 dark:lg:bg-slate-900 lg:border-r-[1px] lg:border-gray-300 dark:lg:border-slate-950 lg:pb-4 lg:flex lg:flex-col justify-between">
       <nav className="mt-4 flex flex-col justify-between">
         <ul role="list" className="flex flex-col items-center space-y-1">
           {routes.map((item) => (
@@ -23,6 +25,14 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition-all"
+        >
+          <Avatar user={user} />
+        </div>
       </nav>
     </div>
   );
