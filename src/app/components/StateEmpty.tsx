feed1a80f0ce492d908d9e0aa711dee08108fca1
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-16 21:28:27 -0300

    creating StateEmpty component

---
+++ src/app/components/StateEmpty.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Text from "../../components/Text";
+
+const StateEmpty = () => {
+  return (
+    <div className="bg-gray-100 px-4 py-10 sm:px-6 lg:px-8 h-full flex justify-center items-center dark:bg-slate-800">
+      <div className="flex flex-col items-center text-center">
+        <Image
+          src="/conversation-animate.svg"
+          alt="conversation animate image"
+          height={400}
+          width={400}
+        />
+        <Text paragraph className="text-2xl font-semibold">
+          Start a new conversation!
+        </Text>
+      </div>
+    </div>
+  );
+};
+
+export default StateEmpty;
