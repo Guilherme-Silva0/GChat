c1bddc390bc46eda6d116ae5384705d09a3869a6
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-18 19:58:07 -0300

    importing users list

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,14 +1,20 @@
+import getUsers from "@/actions/getUsers";
 import Sidebar from "@/app/components/sidebar/Sidebar";
+import UsersList from "./components/UsersList";
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
   return (
     //@ts-expect-error Server Component
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UsersList users={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 }
