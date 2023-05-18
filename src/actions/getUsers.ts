9d92751dd803fafc5098df68b011ab5a77fd696d
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-18 19:53:35 -0300

    get users

---
+++ src/actions/getUsers.ts
@@ -0,0 +1,29 @@
+import prismaClient from "@/libs/prisma";
+import getSession from "./getSession";
+
+const getUsers = async () => {
+  const session = await getSession();
+
+  if (!session?.user?.email) {
+    return [];
+  }
+
+  try {
+    const users = await prismaClient.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        NOT: {
+          email: session.user.email,
+        },
+      },
+    });
+
+    return users;
+  } catch (error) {
+    return [];
+  }
+};
+
+export default getUsers;
