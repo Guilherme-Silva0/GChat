b8e2ea7d0304778fb5d3853bda9a3ab0ab188513
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-18 19:57:09 -0300

    adding className

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -3,7 +3,7 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
-const ThemeButton = () => {
+const ThemeButton = ({ className }: { className?: string }) => {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
@@ -22,7 +22,7 @@ const ThemeButton = () => {
   return (
     <button
       onClick={toggleTheme}
-      className="p-1.5 rounded-lg text-xl text-fist font-black transition-all dark:text-gray-100"
+      className={`p-1.5 rounded-lg text-xl text-fist font-black transition-all dark:text-gray-100 ${className}`}
     >
       {theme === "light" ? <Sun /> : <Moon />}
     </button>
