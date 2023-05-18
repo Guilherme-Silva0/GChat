d08f7f5c49d8727a2fd778cc3c229a090f877ff0
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-18 19:53:26 -0300

    get conversations

---
+++ src/actions/getConversations.ts
@@ -0,0 +1,37 @@
+import prismaClient from "@/libs/prisma";
+import getCurrentUser from "./getCurrentUser";
+
+const getConversations = async () => {
+  const currentUser = await getCurrentUser();
+  if (!currentUser?.id) {
+    return [];
+  }
+
+  try {
+    const conversations = await prismaClient.conversation.findMany({
+      orderBy: {
+        lastMessageAt: "desc",
+      },
+      where: {
+        userIds: {
+          has: currentUser.id,
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            sender: true,
+            seen: true,
+          },
+        },
+      },
+    });
+
+    return conversations;
+  } catch (_error) {
+    return [];
+  }
+};
+
+export default getConversations;
