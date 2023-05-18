17272c27ec4fb531f848ff8bd3be98789084eb3d
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-05-18 19:56:29 -0300

    changing colors

--- src/app/components/sidebar/MobileItem.tsx
+++ src/app/components/sidebar/MobileItem.tsx
@@ -22,8 +22,9 @@ const MobileItem = ({ icon: Icon, href, onClick, active }: MobileItemProps) => {
       onClick={handleClick}
       href={href}
       className={clsx(
-        "group flex gap-x-3 text-sm leading-6 font-semibold rounded-full justify-center p-3 text-gray-500 transition-all hover:text-slate-900 hover:bg-white hover:shadow",
-        active && "bg-white text-slate-900 shadow"
+        "group flex gap-x-3 text-sm leading-6 font-semibold rounded-full justify-center p-3 text-gray-500 transition-all hover:text-gray-200 hover:bg-slate-950 hover:shadow",
+        active &&
+          "bg-white text-slate-900 shadow dark:bg-slate-950 dark:text-gray-200"
       )}
     >
       <Icon className="w-6 h-6" />
