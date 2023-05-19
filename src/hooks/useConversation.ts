00b4dfe570ede3d5e93d9001dc87ef468e1786e0
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-05-19 19:21:28 -0300

    solving bugs

--- src/hooks/useConversation.ts
+++ src/hooks/useConversation.ts
@@ -5,12 +5,12 @@ const useConversation = () => {
   const params = useParams();
 
   const conversationId = useMemo(() => {
-    if (!params?.coversationId) {
+    if (!params?.conversationId) {
       return "";
     }
 
-    return params.coversationId as string;
-  }, [params?.coversationId]);
+    return params.conversationId as string;
+  }, [params?.conversationId]);
 
   const isOpen = useMemo(() => !!conversationId, [conversationId]);
 
