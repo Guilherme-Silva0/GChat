8cca835283e2600444a5b2b3751cf4c8c8be527f
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-05-19 19:23:52 -0300

    get messages

---
+++ src/actions/getMessages.ts
@@ -0,0 +1,24 @@
+import prismaClient from "@/libs/prisma";
+
+const getMessages = async (conversationId: string) => {
+  try {
+    const messages = await prismaClient.message.findMany({
+      where: {
+        conversationId,
+      },
+      include: {
+        sender: true,
+        seen: true,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return messages;
+  } catch (_error) {
+    return [];
+  }
+};
+
+export default getMessages;
