dc1ada7a630a88e7442e1dc541078fe892408a89
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-05-19 19:20:33 -0300

    get conversation by id

---
+++ src/actions/getConversationById.ts
@@ -0,0 +1,27 @@
+import getCurrentUser from "@/actions/getCurrentUser";
+import prismaClient from "@/libs/prisma";
+
+const getConversationsById = async (conversationId: string) => {
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser?.email) {
+      return null;
+    }
+
+    const conversation = await prismaClient.conversation.findUnique({
+      where: {
+        id: conversationId,
+      },
+      include: {
+        users: true,
+      },
+    });
+
+    return conversation;
+  } catch (_error) {
+    return null;
+  }
+};
+
+export default getConversationsById;
