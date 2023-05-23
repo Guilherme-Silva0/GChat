997a2db614a67972c6a7bc120c990fd5e325cc7b
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-05-23 17:00:29 -0300

    object-cover

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -11,6 +11,7 @@ const Avatar = ({ user }: { user?: User }) => {
           alt="Avatar user"
           src={user?.image || '/images/placeholder.jpg'}
           fill
+          className="object-cover"
         />
       </div>
       <span className="absolute right-0 top-0 block h-2 w-2 rounded-full bg-green-600 ring-2 ring-gray-200 dark:ring-slate-900 md:h-3 md:w-3" />
