a3f8a34fa7afd26eaadd6ab82ca011b90226fd85
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-05-25 16:13:31 -0300

    passing user

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -2,6 +2,7 @@ import getUsers from '@/actions/getUsers'
 import Sidebar from '@/components/sidebar/Sidebar'
 import UsersList from './components/UsersList'
 import { ReactNode } from 'react'
+import getCurrentUser from '@/actions/getCurrentUser'
 
 export default async function UsersLayout({
   children,
@@ -9,11 +10,12 @@ export default async function UsersLayout({
   children: ReactNode
 }) {
   const users = await getUsers()
+  const user = await getCurrentUser()
   return (
     // @ts-expect-error Server Component
     <Sidebar>
       <div className="h-full">
-        <UsersList users={users} />
+        <UsersList users={users} user={user!} />
         {children}
       </div>
     </Sidebar>
