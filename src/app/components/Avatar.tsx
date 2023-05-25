b3b8085a21e61bc19cc316282eb91480ebba7fce
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-05-25 16:07:32 -0300

    adding varied sizes

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -3,14 +3,26 @@
 import useActiveList from '@/hooks/useActiveList'
 import { User } from '@prisma/client'
 import Image from 'next/image'
+import clsx from 'clsx'
 
-const Avatar = ({ user }: { user?: User }) => {
+interface AvatarProps {
+  user?: User
+  size?: 'sm' | 'lg'
+}
+
+const Avatar = ({ user, size }: AvatarProps) => {
   const { members } = useActiveList()
   const isActive = members.indexOf(user?.email!) !== -1
 
   return (
     <div className="relative flex items-center">
-      <div className="relative inline-block h-10 w-10 overflow-hidden rounded-full ring-2 ring-sky-600 md:h-11 md:w-11">
+      <div
+        className={clsx(
+          'relative inline-block h-10 w-10 overflow-hidden rounded-full ring-2 ring-sky-600',
+          size === 'sm' && 'h-7 w-7',
+          size === 'lg' && 'h-20 w-20',
+        )}
+      >
         <Image
           alt="Avatar user"
           src={user?.image || '/images/placeholder.jpg'}
@@ -19,7 +31,13 @@ const Avatar = ({ user }: { user?: User }) => {
         />
       </div>
       {isActive && (
-        <span className="absolute right-0 top-0 block h-2 w-2 rounded-full bg-green-600 ring-2 ring-gray-200 dark:ring-slate-900 md:h-3 md:w-3" />
+        <span
+          className={clsx(
+            'absolute right-0 top-0 block h-2 w-2 rounded-full bg-green-600 ring-2 ring-gray-200 dark:ring-slate-900',
+            size === 'sm' && 'h-1.5 w-1.5',
+            size === 'lg' && 'right-1 top-1 h-3 w-3',
+          )}
+        />
       )}
     </div>
   )
