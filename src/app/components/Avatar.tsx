023f484f5791808a48e1068998d3b31f1b3f055f
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-05-24 22:31:09 -0300

    checking if user is online

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -1,9 +1,13 @@
 'use client'
 
+import useActiveList from '@/hooks/useActiveList'
 import { User } from '@prisma/client'
 import Image from 'next/image'
 
 const Avatar = ({ user }: { user?: User }) => {
+  const { members } = useActiveList()
+  const isActive = members.indexOf(user?.email!) !== -1
+
   return (
     <div className="relative flex items-center">
       <div className="relative inline-block h-10 w-10 overflow-hidden rounded-full ring-2 ring-sky-600 md:h-11 md:w-11">
@@ -14,7 +18,9 @@ const Avatar = ({ user }: { user?: User }) => {
           className="object-cover"
         />
       </div>
-      <span className="absolute right-0 top-0 block h-2 w-2 rounded-full bg-green-600 ring-2 ring-gray-200 dark:ring-slate-900 md:h-3 md:w-3" />
+      {isActive && (
+        <span className="absolute right-0 top-0 block h-2 w-2 rounded-full bg-green-600 ring-2 ring-gray-200 dark:ring-slate-900 md:h-3 md:w-3" />
+      )}
     </div>
   )
 }
